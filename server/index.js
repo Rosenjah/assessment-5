dfa11184a7f4d9bf0b6b4f5e15d0b5f464b3b8ad
rose Njambilo <rosenjah@gmail.com>
2022-07-25 16:59:22 -0900

    initial commit

--- server/index.js
+++ server/index.js
@@ -12,20 +12,20 @@ router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/../public/index.html'));
   //__dirname : It will resolve to your project folder.
 });
+app.use('/', router);
 
 app.use(express.json())
 app.use(cors())
-app.use('/', router);
 
 // DEV
 app.post('/seed', seed)
 
 // COUNTRIES
-// app.get('/countries', getCountries)
+app.get('/countries', getCountries)
 
 // CITIES
-// app.post('/cities', createCity)
-// app.get('/cities', getCities)
-// app.delete('/cities/:id', deleteCity)
+app.post('/cities', createCity)
+app.get('/cities', getCities)
+app.delete('/cities/:id', deleteCity)
 
 app.listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}`))
